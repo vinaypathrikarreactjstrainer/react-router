a10c104b97912c859cbed5b8f9f754a0bfff5469
vinaypathrikarreactjstrainer <tinu.vinau2128@gmail.com>
2026-01-20 09:07:47 +0530

    useFormStatus() hook

--- src/protectedRoutes/Contact.jsx
+++ src/protectedRoutes/Contact.jsx
@@ -1,16 +1,49 @@
 import React from "react";
+import { useFormStatus } from "react-dom";
 import UpdatedObj from "../updation/UpdatedObj";
 import UpdatedArray from "../updation/UpdatedArray";
 
+// SignInForm Component
+const SignInForm = () => {
+    const {pending} = useFormStatus();
+    console.log('pending => ', pending);
+  return (
+    <div>
+      <input type="text" placeholder="User Name" />
+      <br />
+      <br />
+      <input type="password" placeholder="Password" />
+      <br />
+      <br />
+      <button type="submit" disabled = {pending ? true : false}>
+        { 
+            pending
+            ? 'Submitting...'
+            : 'Submit'
+        }
+      </button>
+    </div>
+  );
+};
+
 const Contact = () => {
-    return(
-        <>
-            <h1>Contact Page</h1>
-            <UpdatedObj />
-            <hr/>
-            <UpdatedArray />
-        </>
-    )
-}
+  // function
+  const onFormSubmit = async () => {
+    await new Promise((resp) => setTimeout(resp, 5000));
+    console.log("submit..");
+  };
+
+  return (
+    <>
+      <h1>Contact Page</h1>
+      <form action={onFormSubmit}>
+        <SignInForm />
+      </form>
+      {/* <UpdatedObj /> */}
+      <hr />
+      {/* <UpdatedArray /> */}
+    </>
+  );
+};
 
 export default Contact;
